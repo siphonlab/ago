e27ad9e8fe96d90eb6790cd9dd0cca10268e5397
inshua <inshua@gmail.com>
2025-11-13 00:26:35 +0800

    groovy compiler cannot recognize record contstructor
    [ERROR] Groovy:[Static type checking] - Reference to method is ambiguous.
    Cannot choose between [
    	<init>(java.lang.String, long) from org.siphonlab.ago.runtime.rdb.ObjectRef,
    	<init>(java.lang.String, long) from org.siphonlab.ago.runtime.rdb.ObjectRef]

--- ago-engine-rdb/src/main/java/org/siphonlab/ago/runtime/rdb/ObjectRef.java
+++ ago-engine-rdb/src/main/java/org/siphonlab/ago/runtime/rdb/ObjectRef.java
@@ -1,3 +1,38 @@
 package org.siphonlab.ago.runtime.rdb;
 
-public record ObjectRef(String className, long id) {}
+import java.util.Objects;
+
+public final class ObjectRef {
+    private final String className;
+    private final long id;
+
+    public ObjectRef(String className, long id) {
+        this.className = className;
+        this.id = id;
+    }
+
+    public String className() {return className;}
+
+    public long id() {return id;}
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (ObjectRef) obj;
+        return Objects.equals(this.className, that.className) &&
+                this.id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(className, id);
+    }
+
+    @Override
+    public String toString() {
+        return "ObjectRef[" +
+                "className=" + className + ", " +
+                "id=" + id + ']';
+    }
+}
