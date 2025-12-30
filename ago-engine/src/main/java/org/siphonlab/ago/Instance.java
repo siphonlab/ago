4faae22e373ff749db5fa5862c85201335c87193
inshua <inshua@gmail.com>
2025-12-30 13:30:30 +0800

    translator success!

--- ago-engine/src/main/java/org/siphonlab/ago/Instance.java
+++ ago-engine/src/main/java/org/siphonlab/ago/Instance.java
@@ -127,4 +127,85 @@ public class Instance<C extends AgoClass>  {
 //        runSpace.runTillComplete((CallFrame<?>) frame);
 //    }
 
+    public String getStringField(String fieldName){
+        return this.getSlots().getString(this.getAgoClass().findField(fieldName).getSlotIndex());
+    }
+
+    public void setStringField(String fieldName, String value) {
+        this.getSlots().setString(this.getAgoClass().findField(fieldName).getSlotIndex(), value);
+    }
+
+    public int getIntField(String fieldName) {
+        return this.getSlots().getInt(this.getAgoClass().findField(fieldName).getSlotIndex());
+    }
+
+    public void setIntField(String fieldName, int value) {
+        this.getSlots().setInt(this.getAgoClass().findField(fieldName).getSlotIndex(), value);
+    }
+
+    public boolean getBooleanField(String fieldName) {
+        return this.getSlots().getBoolean(this.getAgoClass().findField(fieldName).getSlotIndex());
+    }
+
+    public void setBooleanField(String fieldName, boolean value) {
+        this.getSlots().setBoolean(this.getAgoClass().findField(fieldName).getSlotIndex(), value);
+    }
+
+    public double getDoubleField(String fieldName) {
+        return this.getSlots().getDouble(this.getAgoClass().findField(fieldName).getSlotIndex());
+    }
+
+    public void setDoubleField(String fieldName, double value) {
+        this.getSlots().setDouble(this.getAgoClass().findField(fieldName).getSlotIndex(), value);
+    }
+
+    public float getFloatField(String fieldName) {
+        return this.getSlots().getFloat(this.getAgoClass().findField(fieldName).getSlotIndex());
+    }
+
+    public void setFloatField(String fieldName, float value) {
+        this.getSlots().setFloat(this.getAgoClass().findField(fieldName).getSlotIndex(), value);
+    }
+
+    public long getLongField(String fieldName) {
+        return this.getSlots().getLong(this.getAgoClass().findField(fieldName).getSlotIndex());
+    }
+
+    public void setLongField(String fieldName, long value) {
+        this.getSlots().setLong(this.getAgoClass().findField(fieldName).getSlotIndex(), value);
+    }
+
+    public short getShortField(String fieldName) {
+        return this.getSlots().getShort(this.getAgoClass().findField(fieldName).getSlotIndex());
+    }
+
+    public void setShortField(String fieldName, short value) {
+        this.getSlots().setShort(this.getAgoClass().findField(fieldName).getSlotIndex(), value);
+    }
+
+    public byte getByteField(String fieldName) {
+        return this.getSlots().getByte(this.getAgoClass().findField(fieldName).getSlotIndex());
+    }
+
+    public void setByteField(String fieldName, byte value) {
+        this.getSlots().setByte(this.getAgoClass().findField(fieldName).getSlotIndex(), value);
+    }
+
+    public Instance<?> getObjectField(String fieldName) {
+        return this.getSlots().getObject(this.getAgoClass().findField(fieldName).getSlotIndex());
+    }
+
+    public void setObjectField(String fieldName, Instance<?> value) {
+        this.getSlots().setObject(this.getAgoClass().findField(fieldName).getSlotIndex(), value);
+    }
+
+    public AgoClass getClassRefField(String fieldName) {
+        return this.getAgoClass().getClassLoader().getClass(this.getSlots().getClassRef(this.getAgoClass().findField(fieldName).getSlotIndex()));
+    }
+
+    public void setClassRefField(String fieldName, AgoClass value) {
+        this.getSlots().setClassRef(this.getAgoClass().findField(fieldName).getSlotIndex(), value.classId);
+    }
+
+
 }
