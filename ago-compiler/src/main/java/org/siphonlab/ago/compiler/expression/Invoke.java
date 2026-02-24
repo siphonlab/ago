siphonlab/ago

commit 69b627e24ebbe97204e2f7c417a0f416c93de86f

method of interface(new_voIm) now store the generic instantiated method

ago-compiler/src/main/java/org/siphonlab/ago/compiler/expression/Invoke.java
@@ -257,12 +257,6 @@ public class Invoke extends ExpressionBase{
                 ClassDef scopeClass = scope.inferType();
                 var n = Creator.NewProps.resolve(fun, scopeClass);
                 n.isInterfaceInvoke(isInterfaceInvoke(scopeClass));
-                if(isInterfaceInvoke(scopeClass)){
-                    if(scopeClass.getGenericSource() != null){
-                        // for Interface.method, we use the template interface as scope class
-                        n.setClassName(fun.idOfClass(scopeClass.getTemplateClass()));
-                    }
-                }
                 n.setTraitInScope(scopeClass instanceof TraitDefInScope);
                 code.new_method(resultSlot, result.getVariableSlot(), fun.simpleNameOfFunction(resolvedFunctionDef),
                         n.setForGenericInstantiation(n.forGenericInstantiation() || resolvedFunctionDefGenericInstantiateRequired));

commit fd95ed2160d8638bc56c930164ed940723862dc5

69b627e24ebbe97204e2f7c417a0f416c93de86f broke interface sample

ago-compiler/src/main/java/org/siphonlab/ago/compiler/expression/Invoke.java
@@ -272,6 +272,12 @@ public class Invoke extends ExpressionBase{
                 ClassDef scopeClass = scope.inferType();
                 var n = Creator.NewProps.resolve(fun, scopeClass);
                 n.isInterfaceInvoke(isInterfaceInvoke(scopeClass));
+                if(isInterfaceInvoke(scopeClass)){
+                    if(scopeClass.getGenericSource() != null){
+                        // for Interface.method, we use the template interface as scope class
+                        n.setClassName(fun.idOfClass(scopeClass.getTemplateClass()));
+                    }
+                }
                 n.setTraitInScope(scopeClass instanceof TraitDefInScope);
                 code.new_method(resultSlot, result.getVariableSlot(), fun.simpleNameOfFunction(resolvedFunctionDef),
                         n.setForGenericInstantiation(n.forGenericInstantiation() || resolvedFunctionDefGenericInstantiateRequired));

ago-engine/src/main/java/org/siphonlab/ago/classloader/ClassHeader.java
@@ -1142,9 +1142,9 @@ public class ClassHeader {
             for (int k = 0; k < interfaces.length; k++) {
                 String interface_ = interfaces[k];
                 var interfaceHeader = headers.get(interface_);
-//                if(interfaceHeader.genericSource != null){
-//                    interfaceHeader = interfaceHeader.genericSource.sourceTemplate();
-//                }
+                if(interfaceHeader.genericSource != null){
+                    interfaceHeader = interfaceHeader.genericSource.sourceTemplate();
+                }
                 int[] map = new int[interfaceHeader.methods.size()];
 
                 List<MethodDesc> methodDescs = interfaceHeader.methods;