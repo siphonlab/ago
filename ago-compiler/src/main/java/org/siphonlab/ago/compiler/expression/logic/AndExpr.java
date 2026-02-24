siphonlab/ago

commit 6b97101c8b8802c9c9b8295ad058a94a05a96b75

boolean 逻辑运算结果为 boolean

ago-compiler/src/main/java/org/siphonlab/ago/compiler/expression/logic/AndExpr.java
@@ -15,9 +15,11 @@
  */
 package org.siphonlab.ago.compiler.expression.logic;
 
+import org.siphonlab.ago.TypeCode;
 import org.siphonlab.ago.compiler.BlockCompiler;
 import org.siphonlab.ago.compiler.ClassDef;
 import org.siphonlab.ago.SourceLocation;
+import org.siphonlab.ago.compiler.PrimitiveClassDef;
 import org.siphonlab.ago.compiler.exception.CompilationError;
 
 
@@ -43,6 +45,10 @@ public class AndExpr extends ExpressionBase {
 
     @Override
     protected Expression transformInner() throws CompilationError {
+        if(left.inferType().getUnboxedTypeCode() == TypeCode.BOOLEAN || right.inferType().getUnboxedTypeCode() == TypeCode.BOOLEAN){
+            return new AndExpr(new Cast(left, BOOLEAN, false).transform(), new Cast(right, BOOLEAN, false));
+        }
+
         CastStrategy.UnifyTypeResult result = new CastStrategy(this.getSourceLocation(), false).unifyTypes(this.left, this.right);
         if(result.changed() || result.left() != this.left || result.right() != this.right){
             this.left = result.left();

ago-compiler/src/main/java/org/siphonlab/ago/compiler/expression/logic/OrExpr.java
@@ -15,6 +15,7 @@
  */
 package org.siphonlab.ago.compiler.expression.logic;
 
+import org.siphonlab.ago.TypeCode;
 import org.siphonlab.ago.compiler.BlockCompiler;
 import org.siphonlab.ago.compiler.ClassDef;
 import org.siphonlab.ago.SourceLocation;
@@ -43,6 +44,9 @@ public class OrExpr extends ExpressionBase {
 
     @Override
     protected Expression transformInner() throws CompilationError {
+        if(left.inferType().getUnboxedTypeCode() == TypeCode.BOOLEAN || right.inferType().getUnboxedTypeCode() == TypeCode.BOOLEAN){
+            return new AndExpr(new Cast(left, BOOLEAN, false).transform(), new Cast(right, BOOLEAN, false));
+        }
         CastStrategy.UnifyTypeResult result = new CastStrategy(this.getSourceLocation(), false).unifyTypes(this.left, this.right);
         if (result.changed() || result.left() != this.left || result.right() != this.right) {
             this.left = result.left();

commit c3946f18072b1c893cff6c6304b316c3ca91d4ae

6b97101c8b8802c9c9b8295ad058a94a05a96b75 broke logic sample, `t() and false`

ago-compiler/src/main/java/org/siphonlab/ago/compiler/expression/logic/AndExpr.java
@@ -47,10 +47,14 @@ public class AndExpr extends ExpressionBase {
     protected Expression transformInner() throws CompilationError {
         ClassDef leftType = left.inferType();
         ClassDef rightType = right.inferType();
-        if(leftType.getTypeCode() == TypeCode.BOOLEAN || rightType.getTypeCode() == TypeCode.BOOLEAN
-                || leftType.getUnboxedTypeCode() == TypeCode.BOOLEAN
+        if(leftType.getTypeCode() == TypeCode.BOOLEAN || leftType.getUnboxedTypeCode() == TypeCode.BOOLEAN){
+            if(!(rightType.getTypeCode() == TypeCode.BOOLEAN
+                    || rightType.getUnboxedTypeCode() == TypeCode.BOOLEAN)){
+                return new AndExpr(left, new Cast(right, BOOLEAN, false).transform()).transform();
+            }
+        } else if(rightType.getTypeCode() == TypeCode.BOOLEAN
                 || rightType.getUnboxedTypeCode() == TypeCode.BOOLEAN){
-            return new AndExpr(new Cast(left, BOOLEAN, false).transform(), new Cast(right, BOOLEAN, false));
+            return new AndExpr(new Cast(left, BOOLEAN, false).transform(), right).transform();
         }
 
         CastStrategy.UnifyTypeResult result = new CastStrategy(this.getSourceLocation(), false).unifyTypes(this.left, this.right);

ago-compiler/src/main/java/org/siphonlab/ago/compiler/expression/logic/OrExpr.java
@@ -46,10 +46,14 @@ public class OrExpr extends ExpressionBase {
     protected Expression transformInner() throws CompilationError {
         ClassDef leftType = left.inferType();
         ClassDef rightType = right.inferType();
-        if(leftType.getTypeCode() == TypeCode.BOOLEAN || rightType.getTypeCode() == TypeCode.BOOLEAN
-                || leftType.getUnboxedTypeCode() == TypeCode.BOOLEAN
+        if(leftType.getTypeCode() == TypeCode.BOOLEAN || leftType.getUnboxedTypeCode() == TypeCode.BOOLEAN){
+            if(!(rightType.getTypeCode() == TypeCode.BOOLEAN
+                    || rightType.getUnboxedTypeCode() == TypeCode.BOOLEAN)){
+                return new OrExpr(left, new Cast(right, BOOLEAN, false).transform()).transform();
+            }
+        } else if(rightType.getTypeCode() == TypeCode.BOOLEAN
                 || rightType.getUnboxedTypeCode() == TypeCode.BOOLEAN){
-            return new OrExpr(new Cast(left, BOOLEAN, false).transform(), new Cast(right, BOOLEAN, false));
+            return new OrExpr(new Cast(left, BOOLEAN, false).transform(), right).transform();
         }
         CastStrategy.UnifyTypeResult result = new CastStrategy(this.getSourceLocation(), false).unifyTypes(this.left, this.right);
         if (result.changed() || result.left() != this.left || result.right() != this.right) {