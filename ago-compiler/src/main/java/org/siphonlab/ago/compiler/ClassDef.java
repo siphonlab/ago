cde43e21d8bc0246af3d1768c2039078fed9af4a
inshua <inshua@gmail.com>
2026-08-07 17:57:50 +0800

    `isAffectedByTypeArguments` detect within meta of generic, like ClassHead did

--- ago-compiler/src/main/java/org/siphonlab/ago/compiler/ClassDef.java
+++ ago-compiler/src/main/java/org/siphonlab/ago/compiler/ClassDef.java
@@ -1794,13 +1794,16 @@ public class ClassDef extends ClassContainer {
     public boolean isAffectedByTypeArguments(InstantiationArguments instantiationArguments, Set<ClassDef> visited) {
         if(visited.contains(this)) return false;
         visited.add(this);
-        // TODO different with ClassHeader
         // it makes lang.Function<Meta@<MyLinkedList<string>>.LinkedListNode> undetermined
-        for(ClassDef p = this; p != null; p = p.getParentClass()){
+        for(ClassDef p = this; p != null; ){
             if(p.isGenericTemplate()){
                 var r = instantiationArguments.canApplyOnTemplate(p);
                 if(r) return true;
+            } else if(p instanceof MetaClassDef m){
+                p = m.getInstanceClassDef();
+                continue;
             }
+            p = p.getParentClass();
         }
         if(this.getSuperClass() != null && this.getSuperClass() != this){
             if(this.getSuperClass().isAffectedByTypeArguments(instantiationArguments, visited)) return true;
