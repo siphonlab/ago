5fbcc5598ab1f2c0b6677786135c55b4e4753a53
inshua <inshua@gmail.com>
2026-08-26 16:44:51 +0800

    refactor: remove rowState/usingInstances from SlotsEncoder, output flat slot values directly

--- test-cases/src/test/java/org/siphonlab/ago/json/test/DbJsonTest.java
+++ test-cases/src/test/java/org/siphonlab/ago/json/test/DbJsonTest.java
@@ -28,26 +28,75 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class DbJsonTest {
 
     @Test
-    public void dumpEntity() throws CompilationError, CompilationErrorsException, IOException {
-        Util.run("db_json/dump_entity.ago");
+    public void encodePrimitives() throws CompilationError, CompilationErrorsException, IOException {
+        Util.run("db_json/encode_primitives.ago");
         Trace.printOutput();
+        assertTrue(Trace.outputtedMatch(
+            "\\[.*Alice.*,.*30.*,.*true.*,.*2\\.718.*\\]"
+        ));
     }
 
     @Test
-    public void dumpEntityWithObjectRef() throws CompilationError, CompilationErrorsException, IOException {
-        Util.run("db_json/dump_object_ref.ago");
+    public void encodeObjectRef() throws CompilationError, CompilationErrorsException, IOException {
+        Util.run("db_json/encode_object_ref.ago");
         Trace.printOutput();
+        assertTrue(Trace.outputtedMatch(
+            "\\[.*Beijing.*,.*100000.*\\]",
+            "\\[.*Bob.*,.*25.*,.*Address:\\d+.*\\]"
+        ));
     }
 
     @Test
-    public void dumpArrayInSlot() throws CompilationError, CompilationErrorsException, IOException {
-        Util.run("db_json/dump_array_slot.ago");
+    public void encodeNullable() throws CompilationError, CompilationErrorsException, IOException {
+        Util.run("db_json/encode_nullable.ago");
         Trace.printOutput();
+        assertTrue(Trace.outputtedMatch(
+            "\\[.*Widget.*,.*42.*,.*null.*\\]"
+        ));
     }
 
     @Test
-    public void dumpNullableSlot() throws CompilationError, CompilationErrorsException, IOException {
-        Util.run("db_json/dump_nullable_slot.ago");
+    public void encodeBoxer() throws CompilationError, CompilationErrorsException, IOException {
+        Util.run("db_json/encode_boxer.ago");
         Trace.printOutput();
+        assertTrue(Trace.outputtedMatch(
+            "\\[.*42.*,.*\\{.*type.*lang\\.Integer.*value.*99.*\\}.*\\]"
+        ));
+    }
+
+    @Test
+    public void encodeArray() throws CompilationError, CompilationErrorsException, IOException {
+        Util.run("db_json/encode_array.ago");
+        Trace.printOutput();
+        assertTrue(Trace.outputtedMatch(
+            "\\[.*Alpha.*,.*lang\\.Array<int>:\\d+.*\\]"
+        ));
+    }
+
+    @Test
+    public void encodeList() throws CompilationError, CompilationErrorsException, IOException {
+        Util.run("db_json/encode_list.ago");
+        Trace.printOutput();
+        assertTrue(Trace.outputtedMatch(
+            "\\[.*Q1.*,.*"
+        ));
+    }
+
+    @Test
+    public void encodeMap() throws CompilationError, CompilationErrorsException, IOException {
+        Util.run("db_json/encode_map.ago");
+        Trace.printOutput();
+        assertTrue(Trace.outputtedMatch(
+            "\\[.*\\]"
+        ));
+    }
+
+    @Test
+    public void encodeClassRef() throws CompilationError, CompilationErrorsException, IOException {
+        Util.run("db_json/encode_classref.ago");
+        Trace.printOutput();
+        assertTrue(Trace.outputtedMatch(
+            "\\[.*test.*,.*int.*\\]"
+        ));
     }
 }
